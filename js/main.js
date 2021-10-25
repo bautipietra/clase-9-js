8b24f780a1e45692093c6b2a0ee044a1177eb00c
bautipietra <bautistapietraroia@gmail.com>
2021-10-25 18:48:07 -0300

    correcciones

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-function obtenerDatos() {
+document.getElementById("submit").addEventListener("click", function(){
 
     let valor = Number(document.getElementById('valor').value);
     let tasa = Number(document.getElementById('tasa').value);
 
     document.calculadora.valorObtenido.value = (valor + valor * tasa);
-}
+})
